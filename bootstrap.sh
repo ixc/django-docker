6079e3d4678c96a9b285fe0408cbd4304d936576
Tai Lee <real.human@mrmachine.net>
2016-04-07 12:27:18 +1000

    Get scripts from right URL.

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,9 +35,9 @@ ln -s "/opt/node-v${NODE_VERSION}-linux-x64/bin/npm" /usr/local/bin/
 # Scripts.
 mkdir -p /opt/django/bin
 cd /opt/django/bin
-wget -nv "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/entrypoint-django.sh"
-wget -nv "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/migrate.sh"
-wget -nv "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/setup-local-dev.sh"
-wget -nv "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/supervisor.sh"
+wget -nv "https://raw.githubusercontent.com/ixc/django-docker/${COMMIT}/bin/entrypoint-django.sh"
+wget -nv "https://raw.githubusercontent.com/ixc/django-docker/${COMMIT}/bin/migrate.sh"
+wget -nv "https://raw.githubusercontent.com/ixc/django-docker/${COMMIT}/bin/setup-local-dev.sh"
+wget -nv "https://raw.githubusercontent.com/ixc/django-docker/${COMMIT}/bin/supervisor.sh"
 chmod +x /opt/django/bin/*.sh
 export PATH=/opt/django/bin:$PATH
