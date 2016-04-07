c52e9098e0062553d6c9c5f5d6ef08065d7dcc59
Tai Lee <real.human@mrmachine.net>
2016-04-07 21:50:41 +1000

    Fix accidental non-zero exit code.

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -4,14 +4,14 @@ set -e
 
 cd "${PROJECT_DIR}"
 
-# Create empty initial MD5 signature.
 touch var/migrate.md5
+python manage.py migrate --list > var/migrate.txt
 
-# Migrate.
-if python manage.py migrate --list | md5sum -c --status var/migrate.md5; then
+if md5sum -c --status var/migrate.md5; then
     echo 'Migrations are already up to date. Skip.'
 else
     echo 'Migrations are out of date. Apply.'
     python manage.py migrate --noinput
-    python manage.py migrate --list | md5sum > var/migrate.md5
+    python manage.py migrate --list > var/migrate.txt
+    md5sum var/migrate.txt > var/migrate.md5
 fi
